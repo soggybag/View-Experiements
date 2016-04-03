92a8aa536aa7be02104eadb6baa47d3a6dc19861
Mitchell Hudson <soggybag@gmail.com>
2016-04-02 21:50:41 -0700

    init commit
    Example file for video tutorial

--- View-Experiements/ViewController.swift
+++ View-Experiements/ViewController.swift
@@ -10,9 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let box = UIView()
+    let smallBox = UIView()
+    
+    
+    func onSlider(slider: UISlider) {
+        let n = CGFloat(slider.value)
+        box.frame.size.width = n
+        box.frame.size.height = n
+        
+        // box.bounds.size.width = n
+        // box.bounds.size.height = n
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        box.frame = CGRect(x: 0, y: 0, width: 100, height: 200)
+        self.view.addSubview(box)
+        box.backgroundColor = UIColor.redColor()
+        box.center.x = 375 / 2
+        box.center.y = 200
+        box.clipsToBounds = true
+        
+        smallBox.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
+        smallBox.backgroundColor = UIColor.blueColor()
+        
+        box.addSubview(smallBox)
+        
+        
+        
+        
+        let slider = UISlider()
+        self.view.addSubview(slider)
+        let w = self.view.frame.size.width
+        slider.center.x = w / 2
+        slider.center.y = 400
+        slider.bounds.size.width = w - 40
+        slider.maximumValue = 200
+        slider.addTarget(self, action: #selector(ViewController.onSlider(_:)), forControlEvents: .ValueChanged)
     }
 
     override func didReceiveMemoryWarning() {
